6fa5e62cf240f19f0cc5f797a080a9882d62faa3
imWildCat <wildcat.name@gmail.com>
2018-04-12 13:51:48 +0100

    Remove conflict scheduler test

--- tests/scheduler_test.py
+++ tests/scheduler_test.py
@@ -1,12 +1,11 @@
-from scylla.scheduler import validate_job
 from scylla.validator import Validator
 
 
-def test_validate_job(mocker):
-    mocker.patch('scylla.validator.Validator.validate')
-    v = Validator('127.0.0.1', 80)
-    validate_job(v)
-    v.validate.assert_called_once()
+# def test_validate_job(mocker):
+#     mocker.patch('scylla.validator.Validator.validate')
+#     v = Validator('127.0.0.1', 80)
+#     validate_job(v)
+#     v.validate.assert_called_once()
 
 # FIXME: cannot patch Queue.get
 # def test_validate_ips(mocker):
