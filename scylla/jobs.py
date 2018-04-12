b2453c0e0f4a08bc62f195417acd62f425422c88
imWildCat <wildcat.name@gmail.com>
2018-04-12 14:09:42 +0100

    Create save_ip

--- scylla/jobs.py
+++ scylla/jobs.py
@@ -2,7 +2,19 @@ from scylla.database import ProxyIP
 from scylla.validator import Validator
 
 
+def save_ip(p: ProxyIP):
+    basic_query = ProxyIP.select().where(ProxyIP.ip == p.ip)
+    count = basic_query.count()
+    if count == 0:
+        p.save()
+    else:
+        p_id = ProxyIP.select().get().id
+        p.id = p_id
+        p.save()
+
+
 def validate_proxy_ip(p: ProxyIP):
     v = Validator(host=p.ip, port=int(p.port))
     v.validate()
     # save valid ip into database
+    save_ip(p)
--- tests/jobs_test.py
+++ tests/jobs_test.py
@@ -1,9 +1,24 @@
 from scylla.database import ProxyIP
-from scylla.jobs import validate_proxy_ip
+from scylla.jobs import validate_proxy_ip, save_ip
 
 
 def test_validate_proxy_ip(mocker):
     method = mocker.patch('scylla.validator.Validator.validate')
+    method2 = mocker.patch('scylla.jobs.save_ip')
     p = ProxyIP(ip='127.0.0.1', port=80)
     validate_proxy_ip(p)
     method.assert_called_once()
+    method2.assert_called_once()
+
+
+def test_save_ip():
+    p1 = ProxyIP(ip='192.168.0.1', port=443, latency=200, stability=0.5)
+    save_ip(p1)
+    # basically the same ip
+    p2 = ProxyIP(ip='192.168.0.1', port=443, latency=200, stability=0.5)
+    save_ip(p2)
+    count = ProxyIP.select().where(ProxyIP.ip == '192.168.0.1').count()
+
+    assert count == 1
+
+    ProxyIP.delete().execute()
