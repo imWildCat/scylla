25e4768ef7ae34189be5227663fef9b48a7f16e3
imWildCat <wildcat.name@gmail.com>
2018-04-20 23:26:17 +0100

    Add setup.py

---
+++ README.rst
@@ -0,0 +1,39 @@
+Scylla |Documentation Status|
+=============================
+
+A minimal proxy ip crawler, only supports Python 3.6.
+
+Get started
+-----------
+
+Install directly via pip
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+Use from source
+~~~~~~~~~~~~~~~
+
+.. code:: bash
+
+    git clone https://github.com/imWildCat/scylla.git
+    cd scylla
+    python -m scylla
+
+Roadmap
+-------
+
+-  [ ] RESTFul API
+-  [ ] Programmable API
+-  [ ] Geographical distribution map for proxies
+-  [ ] Docker integration
+-  [ ] Smart web proxy server
+-  [ ] More statistical views of proxies
+-  [ ] Quality statistics for different providers
+
+License
+-------
+
+Apache License 2.0. For more details, please read the
+`LICENSE <./LICENSE>`__ file.
+
+.. |Documentation Status| image:: https://readthedocs.org/projects/scylla-py/badge/?version=latest
+    :target: http://scylla.wildcat.io/en/latest/?badge=latest
---
+++ setup.py
@@ -0,0 +1,39 @@
+import io
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+# Import the README and use it as the long-description.
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name='setup.py',
+    python_requires='>=3.6.0',
+    # If your package is a single module, use this instead of 'packages':
+    packages=['scylla'],
+    version='0.1.0',
+    description='A powerful proxy IP pool',
+    long_description=long_description,
+    author='WildCat',
+    author_email='wildcat.name@gmail.com',
+    url='https://github.com/imWildCat/scylla',
+    download_url='https://github.com/imWildCat/scylla/archive/0.1.0.tar.gz',
+    keywords=['proxy', 'api', 'scylla'],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: Apache Software License'
+    ],
+    install_requires=[
+        'requests',
+        'requests-html',
+        'six',
+        'peewee',
+    ]
+)
