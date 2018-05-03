c1e8560505fb597abfbe7e2b207aedec9cdeeb84
imWildCat <wildcat.name@gmail.com>
2018-05-03 23:36:14 +0100

    Add react-router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,28 @@ import * as ReactDOM from "react-dom";
 import 'react-virtualized/styles.css';
 import 'milligram';
 import "./index.scss";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 import ProxyIPList from "./components/ProxyList";
 
 // import App from "./components/App";
 
+
+const AppRoute = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">ProxyIPList</Link></li>
+            </ul>
+
+            <Route exact path="/" component={ProxyIPList}/>
+        </div>
+    </Router>
+);
+
 ReactDOM.render(
     <div className="container">
-        <ProxyIPList/>
+        <AppRoute/>
     </div>,
     document.getElementById('app')
 );
