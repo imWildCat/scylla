6c889d5b4bdfba3f534dcbfbaf4f15e2624be206
imWildCat <wildcat.name@gmail.com>
2018-05-05 22:41:46 +0100

    Add some map for GeoDistribution

--- frontend/src/components/GeoDistribution.tsx
+++ frontend/src/components/GeoDistribution.tsx
@@ -1,14 +1,35 @@
 import * as React from 'react';
+import {GoogleMap, Marker, withGoogleMap, withScriptjs} from 'react-google-maps';
 
 export interface GeoDistributionProps {
 }
 
+const MapWithAMarker = withScriptjs(withGoogleMap(_ =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{lat: -34.397, lng: 150.644}}
+    >
+        <Marker
+            position={{lat: -34.397, lng: 150.644}}
+        />
+    </GoogleMap>
+));
+
+
 export default class GeoDistribution extends React.Component<GeoDistributionProps, any> {
+
+
     render() {
         return (
             <div>
-                GeoDistribution
+                <MapWithAMarker
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{height: `100%`}}/>}
+                    containerElement={<div style={{height: `400px`}}/>}
+                    mapElement={<div style={{height: `100%`}}/>}
+                />
             </div>
         );
     }
 }
+
