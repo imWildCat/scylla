0d1a44f0e08228fa19ad5bdc66b2915224158d31
imWildCat <wildcat.name@gmail.com>
2018-05-22 18:24:36 +0100

    Re-add cool_proxy_provider_test

--- tests/providers/cool_proxy_provider_test.py
+++ tests/providers/cool_proxy_provider_test.py
@@ -1,3 +1,6 @@
+from scylla.providers import CoolProxyProvider
+from .helpers import assert_provider
+
+
 def test_cool_proxy_provider():
-    pass
-    # assert_provider(CoolProxyProvider())
+    assert_provider(CoolProxyProvider())
