ad5e9ab073bf55317f40ed3542017e60b689c67b
imWildCat <wildcat.name@gmail.com>
2018-05-25 11:28:14 +0100

    Add support for --skip-scheduler flag

--- scylla/cli.py
+++ scylla/cli.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 
-from scylla.config import batch_set_config
+from scylla.config import batch_set_config, get_config
 from scylla.database import create_db_tables
 from scylla.loggings import logger
 from scylla.scheduler import Scheduler
@@ -24,6 +24,8 @@ def main(args) -> int:
                         help='The port number for the web server')
     parser.add_argument('--web-host', '-wh', type=str, default='0.0.0.0',
                         help='The hostname for the web server')
+    parser.add_argument('--skip-scheduler', action='store_true',
+                        help='Prevent the scheduler from crawling')
     parser.add_argument('--version', '-v', action='store_true',
                         help='Print the version of Scylla')
 
@@ -40,10 +42,11 @@ def main(args) -> int:
     s = Scheduler()
 
     try:
-        s.start()
+        if not get_config('skip_scheduler'):
+            s.start()
 
         # web server
-        if not parsed_args_dict['no_webserver']:
+        if not get_config('no_webserver'):
             logger.info('Start the web server')
             start_web_server(
                 host=parsed_args_dict['web_host'], port=parsed_args_dict['web_port'])
