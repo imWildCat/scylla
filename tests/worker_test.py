016fd6c61cca90a81a00cb2f4fd8a26546d50f88
imWildCat <wildcat.name@gmail.com>
2018-05-25 17:11:24 +0100

    Fix worker test

--- tests/worker_test.py
+++ tests/worker_test.py
@@ -8,21 +8,21 @@ def worker_instance():
     return Worker()
 
 
-def test_worker_initialization():
-    worker = worker_instance()
+def test_worker_initialization(worker_instance):
+    worker = worker_instance
     assert worker.session
 
 
-def test_get_html_without_js_rendering():
-    worker = worker_instance()
+def test_get_html_without_js_rendering(worker_instance):
+    worker = worker_instance
     html = worker.get_html('http://www.python-requests.org/', render_js=False).html
     assert '<title>' in html
     assert '<html' in html
     assert '<body>' in html
 
 
-def test_get_html_with_js_rendering():
-    worker = worker_instance()
+def test_get_html_with_js_rendering(worker_instance):
+    worker = worker_instance
     html = worker.get_html('http://example.com/', render_js=True).html
     assert '<title>' in html
     assert '<html' in html
