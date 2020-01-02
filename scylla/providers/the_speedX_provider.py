4747573bb972517d435fe6ebc06822437519da7d
wvengen <dev-net@willem.engen.nl>
2020-01-02 12:47:51 +0100

    Update TheSpeedX provider

--- scylla/providers/the_speedX_provider.py
+++ scylla/providers/the_speedX_provider.py
@@ -4,5 +4,7 @@ class TheSpeedXProvider(PlainTextProvider):
 
     def urls(self) -> [str]:
         return [
-            'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks.txt',
+            'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt',
+            'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt',
+            'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt',
         ]
